de48e2d1a787e08ce2e22957fe30bbbd24e1e10b
Shreyas Ranganatha <shreyass.ranganatha@gmail.com>
2024-12-07 11:05:11 +0530

    day07
    Signed-off-by: Shreyas Ranganatha <shreyass.ranganatha@gmail.com>

---
+++ day07/p2.py
@@ -0,0 +1,26 @@
+
+def dfs(r, vs):
+    if len(vs) <= 1:
+        return vs[0] == r
+
+    a, b = vs.pop(0), vs.pop(0)
+
+    return any([
+        dfs(r, [a+b, *vs]),
+        dfs(r, [a*b, *vs]),
+        dfs(r, [int(str(a) + str(b)), *vs]),
+    ])
+
+with open("inputs/day07.txt") as fp:
+    dt = []
+
+    for ln in fp:
+        r, vs = ln.strip().split(":")
+        dt.append([int(r), [*map(int, vs.strip().split())]])
+
+sm = 0
+for r, vs in dt:
+    if dfs(r, vs.copy()):
+        sm += r
+
+print(sm)
