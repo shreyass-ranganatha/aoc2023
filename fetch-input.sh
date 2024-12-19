952328beb7a3c3233561f88edd8f9eb7bbaa7fa4
Shreyas Ranganatha <shreyass.ranganatha@gmail.com>
2024-12-19 10:30:35 +0530

    Allow day & year args in `fetch-input.sh`
    Signed-off-by: Shreyas Ranganatha <shreyass.ranganatha@gmail.com>

--- fetch-input.sh
+++ fetch-input.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
-
 # A 'cookie' file is necessary
 
-year=`date "+%Y"`
+if [ -z "$1" ]; then
+    day1=`date "+%e" | xargs`
+else
+    day1=$1
+fi
 
-day1=`date "+%e" | xargs`
-day2=`date "+%d"`
+day2=`printf %02d $day1`
+
+if [ -z "$2" ]; then
+    year=`date "+%Y"`
+else
+    year=$2
+fi
 
 cookie=`cat cookie`
 
-curl -o inputs/day${day2}.txt "https://adventofcode.com/${year}/day/${day1}/input" --header "${cookie}"
+curl "https://adventofcode.com/${year}/day/${day1}/input" \
+    -o inputs/day${day2}.txt \
+    --header "${cookie}"
 
-# source files
+# puzzle source
 code day${day2}/p1.py
